3021889446769a2845bf7029c1659d59b60668ca
Nihar Patel <niharmpatel@gmal.com>
2019-11-20 16:58:52 -0500

    init

---
+++ Nihar_Patel_BarrieTransport/NP_BTS_Model.swift
@@ -0,0 +1,22 @@
+//
+//  NP_BTS_Model.swift
+//  Nihar_Patel_BarrieTransport
+//
+//  Created by Nihar Patel on 2019-11-20.
+//  Copyright © 2019 Nihar Patel. All rights reserved.
+//
+
+import Foundation
+
+class NP_BTS_Model {
+    let drivers = [
+        "Alex" : "7052491101",
+        "Bob" : "7052491102",
+        "Chris" : "7052491103",
+        "Don" : "7052491104",
+        "Eric" : "7052491105",
+        "Fred" : "7052491106",
+        "Gerry" : "7052491107"
+    ]
+    
+}
